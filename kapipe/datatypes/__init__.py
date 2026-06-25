4d91acbc42e8dbf35548cb87eb13e0798e9d632c
norikinishida <norikinishida@gmail.com>
2026-06-25 21:16:34 +0900

    Refactor datatype definitions

--- kapipe/datatypes/__init__.py
+++ kapipe/datatypes/__init__.py
@@ -1,25 +1,23 @@
-
 from .datatypes import Config
 
 from .datatypes import Passage
-
 from .datatypes import DocKey
 from .datatypes import Document
+
 from .datatypes import Mention
+
 from .datatypes import Entity
-from .datatypes import Triple
-
 from .datatypes import EntityPage
-
-from .datatypes import CandidateEntitiesForDocument
 from .datatypes import CandEntKeyInfo
+from .datatypes import CandidateEntitiesForDocument
 from .datatypes import EntityPassage
+
+from .datatypes import Triple
 
 from .datatypes import CommunityRecord
 
-from .datatypes import ContextsForOneExample
-
 from .datatypes import QuestionKey
+from .datatypes import ContextsForOneExample
 from .datatypes import Question
 from .datatypes import Answer
 
